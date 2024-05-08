388b9f7bc280cad8bf376bab832d4025739eaa8a
ozz1webdev <ozz1webdev@gmail.com>
2024-05-08 14:41:04 +0200

    adding table

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'flask'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'flasktodo'
-
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:password@localhost/flasktodo'
+db = SQLAlchemy(app)
 
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route("/todo", methods=['GET', 'POST'])
 @app.route("/")
 def index():
     return render_template('index.html')
