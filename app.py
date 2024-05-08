e34e8f2a53e577e8e548adec21533323e9cc2474
ozz1webdev <ozz1webdev@gmail.com>
2024-05-08 08:30:26 +0200

    insert mysql db settings

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'flask'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'flasktodo'
+
+mysql = MySQL(app)
 
 
 @app.route("/")
